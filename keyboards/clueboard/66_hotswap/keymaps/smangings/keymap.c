5523ad31fff9ab40daa8c28e81a0fd2c130f735c
Benjamin Snellings <development@bensnellings.com>
2019-04-18 15:45:48 -0700

    First clueboard keymap

--- keyboards/clueboard/66_hotswap/keymaps/smangings/keymap.c
+++ keyboards/clueboard/66_hotswap/keymaps/smangings/keymap.c
@@ -10,6 +10,8 @@
 #define _ML 8
 #define _LD 9
 
+#define i3_mod KC_LGUI
+
 enum custom_keycodes {
     S_BSKTC = SAFE_RANGE,
     S_ODEJY,
@@ -24,6 +26,7 @@ enum custom_keycodes {
 };
 
 #ifdef AUDIO_ENABLE
+  #define STARTUP_SONG SONG(IMPERIAL_MARCH)
   float song_basketcase[][2] = SONG(BASKET_CASE);
   float song_ode_to_joy[][2]  = SONG(ODE_TO_JOY);
   float song_rock_a_bye_baby[][2]  = SONG(ROCK_A_BYE_BABY);
@@ -35,8 +38,6 @@ enum custom_keycodes {
   float song_zelda_puzzle[][2]  = SONG(ZELDA_PUZZLE);
   float song_imperial_march[][2]  = SONG(IMPERIAL_MARCH);
 #endif
-
-#define i3_mod KC_LGUI
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -54,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FL] = LAYOUT(
   KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,      KC_DEL,            KC_PGUP, \
   _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,_______,_______,_______,MO(_CL),_______,_______,KC_MUTE,                KC_PGDN, \
-  MO(_FL),_______,KC_VOLU,KC_VOLD,KC_MUTE,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,_______,_______,    _______,                             \
+  MO(_FL),_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,_______,_______,    _______,                             \
   _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_PGUP,         \
   _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
 
@@ -62,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_CL] = LAYOUT(
   _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,   _______,            _______, \
-  _______,_______,_______,_______,RESET,  _______,_______,_______,_______,_______,MO(_CL),_______,_______,_______,               _______, \
-  MO(_FL),_______,_______,_______,_______,_______,_______,_______,_______,TG(_LD),_______,_______,    _______,                            \
+  _______,_______,_______,_______,  RESET,  _______,_______,_______,_______,_______,MO(_CL),_______,_______,_______,             _______, \
+  MO(_FL),_______,_______,  DEBUG,_______,_______,_______,_______,_______,TG(_LD),_______,_______,    _______,                            \
   _______,        _______,_______,_______,_______,_______,_______,TG(_ML),_______,_______,_______,        _______,        _______,        \
   _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,_______,_______,_______),
 
@@ -84,19 +85,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX, \
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,    XXXXXXX,                             \
   XXXXXXX,        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,        XXXXXXX,         \
-  XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX),
+  TG(_ML),XXXXXXX,XXXXXXX,                XXXXXXX,XXXXXXX,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX),
 
   /* Keymap _LD:  LED control layer
    */
 [_LD] = LAYOUT(
-  RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_K,RGB_M_X,RGB_M_G,RGB_M_T,RGB_M_SW,RGB_M_SN,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   BL_TOGG,             XXXXXXX, \
-  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                XXXXXXX, \
+  RGB_M_P,RGB_M_B,RGB_M_R,RGB_M_K,RGB_M_X,RGB_M_G,RGB_M_T,RGB_M_SW,RGB_M_SN,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,   BL_TOGG,           BL_INC, \
+  XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                BL_DEC, \
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,    XXXXXXX,                             \
   XXXXXXX,        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,        XXXXXXX,        RGB_SAI,         \
-  XXXXXXX,XXXXXXX,XXXXXXX,                BL_BRTG,BL_BRTG,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,RGB_HUD,RGB_SAD,RGB_HUI),
+  TG(_LD),XXXXXXX,XXXXXXX,                BL_BRTG,BL_BRTG,                        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,RGB_HUD,RGB_SAD,RGB_HUI),
 };
-
-
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -158,7 +157,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case S_IMPER:
                 if (record->event.pressed) {
                     stop_all_notes();
-                    PLAY_SONG(song_zelda_puzzle);
+                    PLAY_SONG(song_imperial_march);
                 }
                 return false;
         #endif
