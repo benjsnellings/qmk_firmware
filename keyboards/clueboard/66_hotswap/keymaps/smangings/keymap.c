4d6846b7f0fcea075fdce0110c07a8c7d4b625d5
Benjamin Snellings <development@bensnellings.com>
2019-04-19 18:47:38 -0700

    Change right space to be ctrl on hold

--- keyboards/clueboard/66_hotswap/keymaps/smangings/keymap.c
+++ keyboards/clueboard/66_hotswap/keymaps/smangings/keymap.c
@@ -18,14 +18,6 @@ void matrix_init_user(void) { // Runs boot tasks for keyboard
   clicky_off();
 };
 
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  // debug_enable=true;
-  // debug_matrix=true;
-  debug_keyboard=true;
-  //debug_mouse=true;
-}
-
 //Tap Dance Declarations
 enum {
   TD_SHIFT = 0
@@ -57,9 +49,8 @@ enum custom_keycodes {
   float song_imperial_march[][2]  = SONG(IMPERIAL_MARCH);
 #endif
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap _BL: Base Layer (Default Layer)
+  /* Keymap _BL: Base Layer Linux
    */
 [_BL] = LAYOUT(
   KC_GESC,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,      KC_BSPC,           KC_LSCR, \
@@ -75,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,                KC_DEL,  \
   MO(_FL),KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,      KC_ENT,                            \
   KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,      TD(TD_SHIFT),        KC_UP,        \
-  KC_LCTL,KC_LGUI,KC_LALT,               LGUI_T(KC_SPC),KC_SPC,                    KC_RGUI,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
+  KC_LCTL,KC_LGUI,KC_LALT,               LGUI_T(KC_SPC),RCTL_T(KC_SPC),            KC_RGUI,KC_RALT,MO(_FL),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT),
 
   /* Keymap _FL: Function Layer
    */
@@ -83,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,      KC_DEL,            KC_PGUP, \
   _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,_______,_______,_______,_______,MO(_CL),_______,_______,_______,                KC_PGDN, \
   MO(_FL),_______,KC_VOLD,KC_VOLU,KC_MUTE,_______,KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,_______,_______,    _______,                             \
-  _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_PGUP,         \
-  _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,KC_HOME,KC_PGDN,KC_END),
+  _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______,        KC_HOME,         \
+  _______,_______,_______,                _______,_______,                        _______,_______,MO(_FL),_______,_______,KC_END ,_______),
 
   /* Keymap _CL: Control layer
    */
