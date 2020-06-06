59fea542a8e13e1a225a552c13bfaef3d1db54c1
Benjamin Snellings <development@bensnellings.com>
2020-06-05 22:18:13 -0700

    Code Cleanup to user space

--- users/smangings/smangings.h
+++ users/smangings/smangings.h
@@ -8,3 +8,33 @@
     Q__NOTE(_AS4), \
     Q__NOTE(_B4 ), \
     HD_NOTE(_C5 )
+
+// Turn into capslock with pressed three times
+void shift_to_caps_down (qk_tap_dance_state_t *state, void *user_data);
+void shift_to_caps_up (qk_tap_dance_state_t *state, void *user_data);
+
+// Space Cadet tap dance 1=() 2={} 3=[]
+void space_cadet_left (qk_tap_dance_state_t *state, void *user_data);
+void space_cadet_left_reset (qk_tap_dance_state_t *state, void *user_data);
+void space_cadet_right (qk_tap_dance_state_t *state, void *user_data);
+void space_cadet_right_reset (qk_tap_dance_state_t *state, void *user_data);
+
+// Make Songs playable
+// enum custom_keycodes;
+
+enum custom_keycodes {
+    S_BSKTC = SAFE_RANGE,
+    S_ODEJY,
+    S_RCKBY,
+    S_DOEDR,
+    S_SCALE,
+    S_ONEUP,
+    S_COIN,
+    S_SONIC,
+    S_ZELDA,
+    S_IMPER,
+    S_RROLL,
+    S_ZELDT
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record);
